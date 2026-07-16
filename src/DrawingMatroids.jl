6f01550cfbab8ec3909c630eda98aacc93f4b51e
Deepak-Mundayur <deepakmsmundayur@gmail.com>
2026-07-16 10:49:09 -0400

    up

--- src/DrawingMatroids.jl
+++ src/DrawingMatroids.jl
@@ -4,7 +4,10 @@ using HomotopyContinuation
 using Plots
 using LinearAlgebra
 
+#import Constrained_Optimization: optimize
 
+include("varieties.jl")
+include("vector_fields.jl")
 include("visualization.jl")
 
 export matroid_visualize
