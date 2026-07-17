dc35e756a1d8cc9a1bab4d680df01a7f678a056a
Deepak-Mundayur <deepakmsmundayur@gmail.com>
2026-07-17 10:30:23 -0400

    up

--- src/DrawingMatroids.jl
+++ src/DrawingMatroids.jl
@@ -4,12 +4,13 @@ using HomotopyContinuation
 using Plots
 using LinearAlgebra
 
-#import Constrained_Optimization: optimize
+import Constrained_Optimization: optimize as constrained_optimize
 
 include("varieties.jl")
 include("vector_fields.jl")
 include("visualization.jl")
 
-export matroid_visualize
+export constrained_optimize
+export matroid_collinearity_system, make_bounded_repelling_force #matroid_visualize
 
 end
